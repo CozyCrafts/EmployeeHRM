99b39d098761726d75d9f726e83042bb98037f34
mean <145245619+meanlego@users.noreply.github.com>
2025-08-29 18:17:16 +0800

    labels

--- EmployeeHRM/EmployeeManagement_frm.vb
+++ EmployeeHRM/EmployeeManagement_frm.vb
@@ -1,3 +1,5 @@
 ﻿Public Class EmployeeManagement_frm
-
+    Private Sub EmployeeManagement_frm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.None
+    End Sub
 End Class
